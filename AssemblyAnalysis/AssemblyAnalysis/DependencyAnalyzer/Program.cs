2895cdeee68f5dd2cedd379ff1c25894a2c147a5
Vincent Lohse <vincent.lohse@live.de>
2021-03-30 14:04:37 +0200

    Added working DependencyAnalyzer.csproj

---
+++ AssemblyAnalysis/AssemblyAnalysis/DependencyAnalyzer/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using NuGet;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet.Packaging;
+
+
+// Inspiration from https://stackoverflow.com/a/6744794/9068781
+namespace DependencyAnalyzer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var analyzerPackagesPath = "/Users/vincent/Desktop/EA Thesis/Code/nuget_packages.txt";
+            var analyzerPackages = File.ReadAllLines(analyzerPackagesPath);
+            var repo = new LocalPackageRepository("/Users/vincent/Desktop/EA Thesis/Code/nuget_analyzer_packages");
+            IQueryable<IPackage> installedPackages = repo.GetPackages();
+            List<IPackage> installedAnalyzerPackages = new List<IPackage>();
+            foreach (IPackage package in installedPackages)
+            {
+                var index = Array.IndexOf(analyzerPackages, package.Id.ToString());
+                if (index > -1)
+                {
+                    installedAnalyzerPackages.Add(package);
+                }
+            }
+            Console.WriteLine($"Number of analyzer packages: {installedAnalyzerPackages.Count()}");
+            OutputGraph(repo, analyzerPackages, installedAnalyzerPackages, 0);
+        }
+
+
+        static void OutputGraph(LocalPackageRepository repository, string [] analyzerPackages, IEnumerable<IPackage> packages, int depth)
+        {
+
+            foreach (IPackage package in packages)
+            {
+                if (package == null)
+                {
+                    Console.WriteLine($"{new string(' ', depth)}Empty package");
+                    continue;
+                }
+
+                Console.WriteLine($"{new string(' ', depth)}{package.Id} v{package.Version}");
+
+                IList<IPackage> dependentPackages = new List<IPackage>();
+                foreach (var dependencySet in package.DependencySets)
+                {
+                    foreach (var dependency in dependencySet.Dependencies)
+                    {
+                        if (dependency.VersionSpec == null)
+                        {
+                            continue;
+                        }
+
+                        var semanticString = dependency.VersionSpec.ToString();
+                        semanticString = semanticString.Replace("[", string.Empty).Replace("]", string.Empty);
+
+                        if (!SemanticVersion.TryParse(semanticString, out SemanticVersion semanticVersion))
+                        {
+                            continue;
+                        }
+
+                        var foundPackage = repository.FindPackage(dependency.Id, semanticVersion);
+                        if (
+                            foundPackage == null ||
+
+                            // Only interested in analyzer package dependencies:
+                            Array.IndexOf(analyzerPackages, foundPackage.Id.ToString()) <= -1
+                        ){
+                            continue;
+                        }
+
+                        dependentPackages.Add(foundPackage);
+                    }
+                }
+
+                // Can be repetitive; Seems like same dependency in different DependencySets?
+                dependentPackages = dependentPackages.Distinct().ToList();
+
+                OutputGraph(repository, analyzerPackages, dependentPackages, depth + 3);
+            }
+        }
+    }
+}
