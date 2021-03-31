a6e560397027f1b1e94691a85b0616d730e45f0f
Vincent Lohse <vincent.lohse@live.de>
2021-03-31 16:48:11 +0200

    Added Python requirements to install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Required:
+# * Python3
 # * C#/Mono
 # * nuget CLI
+
+python3 -m venv ./venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 get_nuget_analyzer_list.py
 
 while read line; do
     # The roslynator tag "--analyzer-assemblies" accepts directories to search in recursively.
@@ -10,14 +16,13 @@ while read line; do
 
     nuget install $line -OutputDirectory nuget_analyzer_packages
     # The package version is included in the directory name. E.g.:
-    #       Microsoft.CodeAnalysis.NetAnalyzers.5.0.3
-    #       StyleCop.Analyzers.1.1.118
+    # --> Microsoft.CodeAnalysis.NetAnalyzers.5.0.3
+    # --> StyleCop.Analyzers.1.1.118
 
 done <nuget_packages.txt
 
 NEW_CSV="True"
-for ANALYZER_PACKAGE in nuget_analyzer_packages/*/
-do
+for ANALYZER_PACKAGE in nuget_analyzer_packages/*/; do
     echo "ANALYZER_PACKAGE: $ANALYZER_PACKAGE"
     # Command for Mac:
     mono AssemblyAnalysis/AssemblyAnalysis/InfoExtractor/bin/Debug/InfoExtractor.exe $ANALYZER_PACKAGE "analyzer_package_details.csv" $NEW_CSV
