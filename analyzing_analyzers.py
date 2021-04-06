8dd0a7f9998c3ca406da07d9b568243c8b29b88b
Vincent Lohse <vincent.lohse@live.de>
2021-04-06 16:56:22 +0200

    Calculated pure_host_packages() & useless_packages()

--- analyzing_analyzers.py
+++ analyzing_analyzers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 def total_das_cps(df):
@@ -65,19 +66,21 @@ def duplicate_diagnostic_ids(df):
         print(cp_duplicates)
 
 
-def unique_source_packages(df, original_packages='nuget_packages.txt'):
+def unique_source_packages(df, print_bool=True):
     """All packages that have their own diagnostic ids, whereby packages
     with multiple versions are only counted once."""
 
     # Not optimal - any dots followed by numbers are removed
-    df = df['HostingPackageName'].str.replace(r'\.\d+', '')
-    df.drop_duplicates(inplace=True)
-    with pd.option_context(
-        'display.min_rows', 70,
-        'display.max_rows', 70,
-        'display.max_colwidth', 300
-    ):
-        print(df)
+    df_hosting_packages = df['HostingPackageName'].str.replace(r'\.\d+', '')
+    df_hosting_packages.drop_duplicates(inplace=True)
+    if print_bool:
+        with pd.option_context(
+            'display.min_rows', 70,
+            'display.max_rows', 70,
+            'display.max_colwidth', 300
+        ):
+            print(df)
+    return df_hosting_packages
 
 
 def missed_packages(df, original_packages='nuget_packages.txt'):
@@ -117,6 +120,91 @@ def missed_packages(df, original_packages='nuget_packages.txt'):
         print(df_missed_packages)
 
 
+def is_hosting(package_name, dependency_dict, source_packages, depth=1):
+    package_name_id = package_name.split("__")[0]
+    # print(f"{' '*depth}package_name_id: {package_name_id}")
+    if package_name_id in source_packages:
+        # print(f"{' '*depth}>>>>>>SOURCE!<<<<<<")
+        return True
+
+    # print(f"{' '*depth}Not a source")
+
+    filtered_dep = dependency_dict[package_name]
+    # print(f"{' '*depth}filtered_dep: {filtered_dep}")
+
+    for package, _ in filtered_dep.items():
+
+        # Go deeper
+        if is_hosting(package, filtered_dep, source_packages, depth + 3):
+            return True
+
+    # print(f"{' '*depth}Not hosting packages")
+    return False
+
+
+def pure_host_packages(df, original_packages='nuget_packages.txt', dependency_json='nuget_deps.json', print_bool=True):
+    """
+    Packages that only host other analyzers without providing own analyzers.
+    They can therefore also be installed as analyzers.
+    """
+    print("Calculating pure host NuGet packages")
+
+    source_packages = unique_source_packages(df, print_bool=False).to_list()
+    with open(original_packages) as f:
+        nuget_packages = [x.rstrip() for x in f]  # remove line breaks
+
+    # Could also be hosting nothing:
+    potential_host_packages = list(set(nuget_packages) - set(source_packages))
+    # --> e.g. [Apex.Analyzers.Immutable.Semantics, Thor.Analyzer.Legacy, CESCodeAnalyzerTest]
+
+    with open(dependency_json) as json_file:
+        dependency_structure = json.load(json_file)
+
+    all_versioned_packages = dependency_structure.keys()
+    # --> e.g. [Apex.Analyzers.Immutable.Semantics__1.1, Thor.Analyzer.Legacy__3.2, CESCodeAnalyzerTest__1.0]
+
+    potential_host_packages_versioned = []
+    for host_package in potential_host_packages:
+        for versioned_package in all_versioned_packages:
+            versioned_package_id = versioned_package.split("__")[0]
+            if host_package == versioned_package_id:
+                potential_host_packages_versioned.append(versioned_package)
+
+    host_packages = [package for package in potential_host_packages_versioned if is_hosting(
+        package, dependency_structure, source_packages)]
+
+    if print_bool:
+        for package in host_packages:
+            print(package)
+        print("Number host_packages: ", len(host_packages))
+
+    return host_packages
+
+
+def useless_packages(df, original_packages='nuget_packages.txt', dependency_json='nuget_deps.json', print_bool=True):
+    """
+    Packages that neither host other analyzers nor provide own analyzers;
+    Practically irrelevant packages.
+    """
+    print("Calculating useless NuGet packages - neither host not source.")
+
+    with open(original_packages) as f:
+        nuget_packages = [x.rstrip() for x in f]
+
+    source_packages = unique_source_packages(df, print_bool=False).to_list()
+    host_packages = pure_host_packages(df, print_bool=False)
+
+    useless_packages = list(set(nuget_packages) -
+                            set(source_packages) - set(host_packages))
+
+    if print_bool:
+        for package in useless_packages:
+            print(package)
+        print("Number useless_packages: ", len(useless_packages))
+
+    return useless_packages
+
+
 def calculate_analyzer_statistics(csv_file="analyzer_package_details.csv"):
 
     df = pd.read_csv(csv_file)
@@ -127,8 +215,10 @@ def calculate_analyzer_statistics(csv_file="analyzer_package_details.csv"):
 
     # duplicate_diagnostic_ids(df)
 
-    unique_source_packages(df)
+    # unique_source_packages(df)
     # missed_packages(df)
+    # pure_host_packages(df)
+    useless_packages(df)
 
     # TODO: Find
     # 1. Percentage of diagnostic_analyzers that have a codefix_provider
