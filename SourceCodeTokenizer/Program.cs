ed917a4961d55361c066db97ec84858b90b93ea4
Vincent Lohse <vincent.lohse@live.de>
2021-05-06 19:34:04 +0200

    Removed comments

--- SourceCodeTokenizer/Program.cs
+++ SourceCodeTokenizer/Program.cs
@@ -16,29 +16,17 @@ namespace SourceCodeTokenizer
     {
 
         /*
-        Previously:
+        Original input:
           input_file: commit_data.jsonl
           output_file: github_commits.dataset.jsonl
           grammar_file: grammar.full.json
 
-        Single entry (one line) in commit_data.jsonl:
+        whereby, single entry (one line) in commit_data.jsonl:
           entry = OrderedDict(id=revision_id, prev_file=prev_file_content, updated_file=updated_file_content)
-
-        Problem:
-            - How to parse diagnostic message? e.g. 'Replace DateTime usage with DateTimeOffset'
-            - Also need to tokenize all ParsedDiff.Action.TargetLines
-
-        TODO Change in unifying_raw_dataset.py:
-            - Add 'requiredLinesStart' and 'requiredLinesEnd'
-            - Keep 'FileContext' for now for basic comparison
-            - 
 
         New input:
             - folder with all json files (output is same)
             - grammar file (?)
-
-        Other input: <grammar_file>
-
         */
 
         private static void Main(string[] args)
