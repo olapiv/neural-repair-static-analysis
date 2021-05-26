484370ee4ce1eadcf408e55e653e611cd96eadae
Vincent Lohse <vincent.lohse@live.de>
2021-05-26 22:42:02 +0200

    Adding context tokens to core_tokens

--- tokenizing_unified_dataset.py
+++ tokenizing_unified_dataset.py
@@ -32,6 +32,52 @@ def remove_redundant_fields(data_dict):
 
     return
 
+
+def split_tokens_by_line(orig_file_tokens):
+    orig_file_line_tokens = [[]]
+    idx = 0
+    for (token_type, value) in orig_file_tokens:
+        orig_file_line_tokens[idx].append((token_type, value))
+        if value == "NEWLINE":
+            orig_file_line_tokens.append([])
+            idx += 1
+
+    return orig_file_line_tokens
+
+
+def count_tokens_in_lines(all_tokens, start_line, num_lines):
+    line_tokens = split_tokens_by_line(all_tokens)[start_line:start_line+num_lines]
+    num_tokens = 0
+    for line in line_tokens:
+        num_tokens += len(line)
+    return num_tokens
+
+def add_context_to_tokens(all_tokens, core_token_list, core_idx_start, num_total_tokens=TOKENS_PER_DATAPOINT):
+
+    context_token_list = core_token_list.copy()
+    max_idx = len(all_tokens) - 1
+    core_idx_end = core_idx_start + len(context_token_list)
+    num_tokens_to_add = num_total_tokens - len(context_token_list)
+    
+    while num_tokens_to_add > 0:
+
+        if core_idx_start == 0 and core_idx_end == max_idx:
+            break
+
+        if core_idx_start == 0 or num_tokens_to_add % 2 == 1:
+            core_idx_end += 1
+            context_token_list.append(all_tokens[core_idx_end])
+
+        else:
+        # elif core_idx_end == max_idx or num_tokens_to_add % 2 == 0:
+            core_idx_start -= 1
+            context_token_list.insert(0, all_tokens[core_idx_start])
+
+        num_tokens_to_add -= 1
+    
+    return context_token_list
+
+
 def main():
     the_lexer = CSharpAndCommentsLexer()
 
@@ -46,43 +92,42 @@ def main():
         with open(path_to_file, 'r') as file:
             orig_file_string = file.read()
         num_lines = orig_file_string.count('\n')
-        orig_file_tokens = the_lexer.get_tokens(orig_file_string)
-        
-        orig_file_line_tokens = [[]]
-        idx = 0
-        for (token_type, value) in orig_file_tokens:
-            orig_file_line_tokens[idx].append((token_type, value))
-            if value == "NEWLINE":
-                orig_file_line_tokens.append([])
-                idx += 1
+        orig_file_tokens = [
+            result for result in the_lexer.get_tokens(orig_file_string)]
 
         # Because lexer always adds NEWLINE at very end
-        del orig_file_line_tokens[-1]
-        del orig_file_line_tokens[-1][-1]
-        assert num_lines == len(orig_file_line_tokens), "num_lines not equal to len(orig_file_line_tokens)"
+        del orig_file_tokens[-1]
 
-        # print("tokens first line: ", orig_file_line_tokens[0])
-        print("tokens last line: ", orig_file_line_tokens[-1])
+        ### Get required original tokens ###
+
+        orig_file_line_tokens = split_tokens_by_line(orig_file_tokens)
+        assert num_lines == len(
+            orig_file_line_tokens), "num_lines not equal to len(orig_file_line_tokens)"
 
         # Diff indices start at 1
         start_required_index = unified_data_dict["RequiredLinesStart"] - 1
+        # Diff indices start at 1
         end_required_index = unified_data_dict["RequiredLinesEnd"] - 1
         orig_required_line_tokens = orig_file_line_tokens[start_required_index:end_required_index + 1]
-        
-        # print("tokens first line: ", orig_required_line_tokens[0])
-        # print("tokens last line: ", orig_required_line_tokens[-1])
 
+        # Flatten tokens again:
         orig_required_tokens = []
         for line_tokens in orig_required_line_tokens:
             for token in line_tokens:
                 orig_required_tokens.append(token)
-        
+
         if len(orig_required_tokens) > TOKENS_PER_DATAPOINT:
             print("Too many required tokens: ", len(orig_required_tokens))
 
+        ### Get extra context (original) tokens ###
+
+        token_index_required_start = count_tokens_in_lines(orig_file_tokens, 0, start_required_index)
+        orig_padded_tokens = add_context_to_tokens(orig_file_tokens, orig_required_tokens, token_index_required_start)
+        assert len(orig_padded_tokens) <= TOKENS_PER_DATAPOINT, f"Too many context tokens: {len(orig_padded_tokens)}"
+        assert len(orig_padded_tokens) >= len(orig_required_tokens), f"Too few context tokens: {len(orig_padded_tokens)}"
+
 
         # TODO:
-        # 1. Add additional tokens until 100(?) are reached
         # 1. Subtract line number of file context (offset) from diff src code locations
         # 1. Optional: Index vars
         # ------
@@ -94,7 +139,6 @@ def main():
         # 1. Optional: Index vars
         # ------
         # 1. Add Error token...?
-        
 
         remove_redundant_fields(unified_data_dict)
 
