d828b0d28557abbac769afdc4f0ca0152115a808
Vincent Lohse <vincent.lohse@live.de>
2021-05-30 21:11:19 +0200

    Added tests for attribute, class, where & newlines

--- regex_lexer_tests.py
+++ regex_lexer_tests.py
@@ -1,5 +1,8 @@
 import unittest
+import requests
+import json
 from regex_lexer import CSharpAndCommentsLexer
+from tokenizing_unified_dataset import split_tokens_by_line
 
 
 the_lexer = CSharpAndCommentsLexer()
@@ -144,8 +147,8 @@ Redirect = 302,"""
     def test_pragma(self):
 
         test_string = """#pragma warning disable 436 // SuppressUnmanagedCodeSecurityAttribute defined in source and mscorlib"""
-        true_token_list = ["#pragma", "WHITESPACE", "warning", "WHITESPACE", "disable", "WHITESPACE", "4", "3", "6", "WHITESPACE", "//", "WHITESPACE", "SuppressUnmanagedCodeSecurityAttribute", "WHITESPACE", 
-                            "defined", "WHITESPACE", "in", "WHITESPACE", "source", "WHITESPACE", "and", "WHITESPACE", "mscorlib", "NEWLINE"]
+        true_token_list = ["#pragma", "WHITESPACE", "warning", "WHITESPACE", "disable", "WHITESPACE", "4", "3", "6", "WHITESPACE", "//", "WHITESPACE", "SuppressUnmanagedCodeSecurityAttribute", "WHITESPACE",
+                           "defined", "WHITESPACE", "in", "WHITESPACE", "source", "WHITESPACE", "and", "WHITESPACE", "mscorlib", "NEWLINE"]
         self.run_single_test(test_string, true_token_list)
 
     def test_attribute_simple(self):
@@ -154,7 +157,8 @@ Redirect = 302,"""
 
     [Flags]
     internal"""
-        true_token_list = ["}", "NEWLINE", "NEWLINE", ["WHITESPACE"]*4, "[", "Flags", "]", "NEWLINE", ["WHITESPACE"]*4, "internal", "NEWLINE"]
+        true_token_list = ["}", "NEWLINE", "NEWLINE", [
+            "WHITESPACE"]*4, "[", "Flags", "]", "NEWLINE", ["WHITESPACE"]*4, "internal", "NEWLINE"]
         self.run_single_test(test_string, true_token_list)
 
     def test_attribute_complex(self):
@@ -163,13 +167,82 @@ Redirect = 302,"""
         test_string = """{
     [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("bla"), SuppressUnmanagedCodeSecurity]
     internal interface ISymNGenWriter"""
-        true_token_list = ["{", "NEWLINE", ["WHITESPACE"]*4, "[", "ComImport", ",", "WHITESPACE", "InterfaceType", "(", 
-        "ComInterfaceType", ".", "InterfaceIsIUnknown", ")", ",", "WHITESPACE",
-        "Guid", "(", "\"", "bla", "\"", ")", ",", "WHITESPACE", "SuppressUnmanagedCodeSecurity", "]",
-        "NEWLINE", ["WHITESPACE"]*4, "internal", "WHITESPACE", "interface", "WHITESPACE", "ISymNGenWriter", "NEWLINE"]
+        true_token_list = ["{", "NEWLINE", ["WHITESPACE"]*4, "[", "ComImport", ",", "WHITESPACE", "InterfaceType", "(",
+                           "ComInterfaceType", ".", "InterfaceIsIUnknown", ")", ",", "WHITESPACE",
+                           "Guid", "(", "\"", "bla", "\"", ")", ",", "WHITESPACE", "SuppressUnmanagedCodeSecurity", "]",
+                           "NEWLINE", ["WHITESPACE"]*4, "internal", "WHITESPACE", "interface", "WHITESPACE", "ISymNGenWriter", "NEWLINE"]
         self.run_single_test(test_string, true_token_list)
 
-    # TODO: UUID ?
+    def test_basic_class(self):
+
+        test_string = """class Xyz {
+    }"""
+        true_token_list = ["class", "WHITESPACE", "Xyz", "WHITESPACE", "{", "NEWLINE", ["WHITESPACE"]*4, "}", "NEWLINE"]
+        self.run_single_test(test_string, true_token_list)
+
+    def test_where_keyword(self):
+
+        test_string = """public partial struct Nullable<T> where T : struct
+    {
+        c T value;"""
+        true_token_list = ["public", "WHITESPACE", "partial", "WHITESPACE", "struct", "WHITESPACE", "Nullable",
+                           "<", "T", ">", "WHITESPACE", "where", "WHITESPACE", "T", "WHITESPACE", ":", "WHITESPACE",
+                           "struct", "NEWLINE", [
+                               "WHITESPACE"]*4, "{", "NEWLINE", ["WHITESPACE"]*8, "c",
+                           "WHITESPACE", "T", "WHITESPACE", "value", ";", "NEWLINE"]
+        self.run_single_test(test_string, true_token_list)
+
+    def test_count_newlines(self):
+        """
+        Testing on large files; simply checking whether all NEWLINEs are counted correctly
+        """
+        # file_to_test = "https://raw.githubusercontent.com/dotnet/runtime/e98d043d7d293c88a346b632d8fc12564a8ef0ce/src/coreclr/tools/aot/ILCompiler.Diagnostics/ISymNGenWriter.cs"
+        # file_to_test = "https://raw.githubusercontent.com/dotnet/runtime/e98d043d7d293c88a346b632d8fc12564a8ef0ce/src/coreclr/tools/aot/ILCompiler.Diagnostics/MethodInfo.cs"
+        file_to_test = "https://raw.githubusercontent.com/dotnet/runtime/e98d043d7d293c88a346b632d8fc12564a8ef0ce/src/libraries/System.Runtime/ref/System.Runtime.cs"
+        f = requests.get(file_to_test)
+        file_string = f.text.rstrip('\n')  # file.text returns non-existent newline at the end...
+        num_total_lines = file_string.count('\n') + 1  # +1 because logic of newlines
+
+        line_deltas = 40
+        line_start = 0
+        while line_start < num_total_lines:
+
+            end_of_file = (line_start + line_deltas) > num_total_lines
+
+            if end_of_file:
+                split_file = file_string.split(
+                    "\n")[line_start:]
+            else:
+                split_file = file_string.split(
+                    "\n")[line_start:line_start+line_deltas]
+
+            file_string_section = "\n".join(split_file)
+
+            orig_file_tokens = [
+                result for result in the_lexer.get_tokens(file_string_section)]
+
+            # Because lexer always adds NEWLINE at very end
+            del orig_file_tokens[-1]
+
+            line_tokens = split_tokens_by_line(orig_file_tokens)
+
+            if len(split_file) != len(line_tokens):
+                for count, line in enumerate(line_tokens):
+                    for token in line:
+                        if token[1] != "WHITESPACE":
+                            first_useful_token = token
+                            break
+                    print(
+                        f"[0]:{first_useful_token} [-1]:{line[-1][1]} content: {split_file[count]}")
+
+                self.assertEqual(line_deltas, len(line_tokens), f"""
+                Between line {line_start} and line {line_start + line_deltas},
+                line_deltas: {line_deltas}; len(line_tokens): {len(line_tokens)}; Not equal!""")
+
+            line_start += line_deltas
+
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
