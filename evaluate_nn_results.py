cf0630e0f9343843b1998dfdba0063a6edb75d43
Vincent Lohse <vincent.lohse@live.de>
2021-06-04 17:26:10 +0200

    Basic calculations #correct, #copied, #extrapolated results

--- evaluate_nn_results.py
+++ evaluate_nn_results.py
@@ -28,7 +28,9 @@ evaluation_dict = {
     "num_total_datapoints": 0,
     "num_extrapolated_datapoints": 0,
 
+    # Hard to do this per diagnostic, since some may have been both correct and incorrect
     "correct_results_total_perc": 0,
+    "correct_results_copied_perc": 0,
     "correct_results_extrapolated_perc": 0,
 
     # Take different DiagnosticIDs for examples here
@@ -50,12 +52,12 @@ evaluation_dict = {
     ],
 
     "result_per_diagnostic": {
-        "DA2001": {
-            "perc_correct": 0,
-            "correct": 0,
-            "wrong": 0,
-            "included_in_training": True
-        }
+        # "DA2001": {
+        #     "perc_correct": 0,
+        #     "correct": 0,
+        #     "wrong": 0,
+        #     "included_in_training": True
+        # }
     }
 }
 
@@ -81,11 +83,77 @@ def main():
     with open(metadata_test_file) as json_file:
         metadata_test = json.load(json_file)
 
+    with open(metadata_train_file) as json_file:
+        metadata_train = json.load(json_file)
+
+    with open(src_test_file, 'r') as file:
+        src_test_list = file.read().split("\n")
+
     with open(tgt_test_file, 'r') as file:
-        tgt_test_string = file.read()
+        tgt_test_list = file.read().split("\n")
 
     with open(inference_test_file, 'r') as file:
-        inference_test_string = file.read()
+        inference_test_list = file.read().split("\n")
+
+    evaluation_dict["num_total_datapoints"] = len(inference_test_list)
+
+    for index, tgt_test_line in enumerate(tgt_test_list):
+        is_correct = tgt_test_line == inference_test_list[index]
+
+        diagnostic_id = metadata_test["datapoints"][index] # ID, DiagnosticID
+
+        included_in_training = diagnostic_id in metadata_train["diagnostics"].keys()
+        if diagnostic_id not in evaluation_dict["result_per_diagnostic"]:
+
+            evaluation_dict["result_per_diagnostic"][diagnostic_id] = {
+                "perc_correct": 0,  # Calculate later
+                "correct": 1 if is_correct else 0,
+                "wrong": 0 if is_correct else 1,
+                "included_in_training": included_in_training
+            }
+
+        else:
+            if is_correct:
+                evaluation_dict["result_per_diagnostic"][diagnostic_id]["correct"] =+ 1
+            else:
+                evaluation_dict["result_per_diagnostic"][diagnostic_id]["wrong"] =+ 1
+
+        if not included_in_training:
+            if is_correct:
+                evaluation_dict["correctly_extrapolated_examples"] = "..."
+            else:
+                evaluation_dict["incorrectly_extrapolated_examples"] = "..."
+
+    total_total = 0
+    copied_total = 0
+    extrapolated_total = 0
+
+    total_correct = 0
+    copied_correct = 0
+    extrapolated_correct = 0
+
+    for value in evaluation_dict["result_per_diagnostic"].values():
+        total_datapoints = value["correct"] + value["wrong"]
+        value["perc_correct"] = value["correct"] / total_datapoints
+
+        total_total += total_datapoints
+        total_correct += value["correct"]
+
+        if value["included_in_training"]:
+            copied_correct += value["correct"]
+            copied_total += total_datapoints
+        else:
+            extrapolated_correct += value["correct"]
+            extrapolated_total += total_datapoints
+    
+    evaluation_dict["correct_results_total_perc"] = total_correct / total_datapoints
+    evaluation_dict["correct_results_copied_perc"] = copied_correct / copied_total
+    evaluation_dict["correct_results_extrapolated_perc"] = extrapolated_correct / extrapolated_total
+
+
+    with open(inference_eval_file, 'w') as fout:
+        json_str = json.dumps(evaluation_dict, indent=4)
+        print(json_str, file=fout)
 
 
 if __name__ == '__main__':
