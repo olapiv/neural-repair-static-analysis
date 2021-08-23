0e30362af2b218dde262114a74668b2da96d0366
Vincent Lohse <vincent.lohse@live.de>
2021-08-23 17:16:52 +0200

    Created bash script to create opennmt-py yml file and run model

---
+++ experiment/run_opennmt_py_transformer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    exit 1
+fi
+
+# Any of imitate__100_tokens__standard__3, imitate__150_tokens__camelcase__3, etc.
+DATASET_NAME=$1
+
+MODEL_NAME="opennmt_py__transformer"
+
+NEW_CONFIG_FILE_NAME="${DATASET_NAME}__${MODEL_NAME}.yml"
+
+# Create copy of config file
+cp opennmt_py_transformer_config.yml $NEW_CONFIG_FILE_NAME
+
+RELATIVE_OUTPUT_DIR="${DATASET_NAME}\/${MODEL_NAME}"
+
+# Insert correct paths
+sed -i'.original' -e "s/INSERT-DATA-DIR/${DATASET_NAME}/" $NEW_CONFIG_FILE_NAME
+rm -f "${NEW_CONFIG_FILE_NAME}.original"
+sed -i'.original' -e "s/INSERT-OUTPUT-DIR/${RELATIVE_OUTPUT_DIR}/" $NEW_CONFIG_FILE_NAME
+rm -f "${NEW_CONFIG_FILE_NAME}.original"
+
+source venv_opennmt_py/bin/activate
+
